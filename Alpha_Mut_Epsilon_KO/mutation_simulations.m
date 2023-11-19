9e281afd4cc7f2877818a05af4a250368838411d
apekshasingh <50004909+apekshasingh@users.noreply.github.com>
2023-11-18 19:07:47 -0800

    updated mutation simulations

--- Alpha_Mut_Epsilon_KO/mutation_simulations.m
+++ Alpha_Mut_Epsilon_KO/mutation_simulations.m
@@ -20,17 +20,17 @@ run_r84=false;
 alpha_mut = false;
 epsilon_KO = false;
 modify_IkBeRelA = false;
-modify_IKKIkBe = false;
+modify_IKKIkBe = true;
 %% Specify Plotting Options
-plot_expt = true;
-plot_sim = false;
+plot_expt = false;
+plot_sim = true;
 %% Plotting experimental data and model simulations
 length_t = 12;
 
-untran_r = 0.100;
-untran_c = 0.025;
+untran_r = 0.130;
+untran_c = 0.035;
 
-names = {'IkBaRelA','IkBeRelA','IKKIkBaRelA','IKKIkBeRelA','RelA','IkBaRelAn','IkBeRelAn','RelAn','IkBacRel','IkBecRel','IKKIkBacRel','IKKIkBecRel','cRel','IkBacReln','IkBecReln','cReln','IKK'};
+names = {'IkBaRelA','IkBeRelA','IKKIkBaRelA','IKKIkBeRelA','RelA','IkBaRelAn','IkBeRelAn','RelAn','IkBacRel','IkBecRel','IKKIkBacRel','IKKIkBecRel','cRel','IkBacReln','IkBecReln','cReln'};
 options = struct;
 options.DEBUG = 1;
 options.SIM_TIME = length_t*60;
@@ -53,10 +53,9 @@ if run_TNF
             6 1 1.5e-5];
         expt = readmatrix('alphamut_TNF_traj.csv');
         expt = expt(1:2:end, :);
-    elseif epsilon_KO %set basal and induced synthesis to zero (hence no IkBe in system)
+    elseif epsilon_KO %set translation to zero (hence no IkBe in system)
         p_mod = [
-            27 1 0
-            28 1 0];
+            30 1 0];
         expt = readmatrix('epsilonKO_TNF_traj.csv');
         expt = expt(1:2:end, :);
     elseif modify_IkBeRelA %original dissociation 0.08 (KD = 0.0014 to KD = 6.3937e-07)
@@ -108,6 +107,7 @@ if run_TNF
     rela(:,1) = rela_nuc;
     rela_total = (3.5*rela_cyto+rela_nuc)/4.5;
     rela_ratio(:,1) = rela_nuc/(rela_total(1)+untran_r);
+    rela_ratio(:,1) = rela_ratio(:,1) - rela_ratio(1,1);
 
     %cRel Calculations
     crel = [];
@@ -117,6 +117,7 @@ if run_TNF
     crel(:,1) = crel_nuc;
     crel_total = (3.5*crel_cyto+crel_nuc)/4.5;
     crel_ratio(:,1) = crel_nuc/(crel_total(1)+untran_c);
+    crel_ratio(:,1) = crel_ratio(:,1) - crel_ratio(1,1);
 
     %legend('RelA ratio (sim)','cRel ratio (sim)', 'RelA ratio', 'cRel ratio')
     title('TNF Simulation')
@@ -141,10 +142,9 @@ if run_lps
             6 1 1.5e-5];
         expt = readmatrix('alphamut_LPS_traj.csv');
         expt = expt(1:2:end, :);
-    elseif epsilon_KO %set basal and induced synthesis to zero (hence no IkBe in system)
+    elseif epsilon_KO %set translation to zero (hence no IkBe in system)
         p_mod = [
-            27 1 0
-            28 1 0];
+            30 1 0];
         expt = readmatrix('epsilonKO_LPS_traj.csv');
         expt = expt(1:2:end, :);
     elseif modify_IkBeRelA
@@ -185,6 +185,7 @@ if run_lps
     rela(:,2) = rela_nuc;
     rela_total = (3.5*rela_cyto+rela_nuc)/4.5;
     rela_ratio(:,2) = rela_nuc/(rela_total(1)+untran_r);
+    rela_ratio(:,2) = rela_ratio(:,2) - rela_ratio(1,2);
 
     %cRel Calculations
     crel_cyto = sum(output_lps(:,9:13),2);
@@ -192,6 +193,7 @@ if run_lps
     crel(:,2) = crel_nuc;
     crel_total = (3.5*crel_cyto+crel_nuc)/4.5;
     crel_ratio(:,2) = crel_nuc/(crel_total(1)+untran_c);
+    crel_ratio(:,2) = crel_ratio(:,2) - crel_ratio(1,2);
 
     figure
     hold on
@@ -223,10 +225,9 @@ if run_cpg
     if alpha_mut %reduced IkBa induced synthesis by Rela
         p_mod = [
             6 1 1.5e-5];
-    elseif epsilon_KO %set basal and induced synthesis to zero (hence no IkBe in system)
+    elseif epsilon_KO %set translation to zero (hence no IkBe in system)
         p_mod = [
-            27 1 0
-            28 1 0];
+            30 1 0];
     elseif modify_IkBeRelA
         p_mod = [
             59 1 0.00003576
@@ -264,6 +265,7 @@ if run_cpg
     rela(:,3) = rela_nuc;
     rela_total = (3.5*rela_cyto+rela_nuc)/4.5;
     rela_ratio(:,3) = rela_nuc/(rela_total(1)+untran_r);
+    rela_ratio(:,3) = rela_ratio(:,3) - rela_ratio(1,3);
 
     %cRel Calculations
     crel_cyto = sum(output_cpg(:,9:13),2);
@@ -271,11 +273,12 @@ if run_cpg
     crel(:,3) = crel_nuc;
     crel_total = (3.5*crel_cyto+crel_nuc)/4.5;
     crel_ratio(:,3) = crel_nuc/(crel_total(1)+untran_c);
+    crel_ratio(:,3) = crel_ratio(:,3) - crel_ratio(1,3);
     
     title('CpG Simulation')
     xlabel('Time(hr)')
     ylabel('Nuclear/Total Cell Intensity')
-    axis([0 options.SIM_TIME/60 0 2.5])
+    axis([0 options.SIM_TIME/60 0 2])
 
     plot(time, rela_ratio(:,3),'Color',sscanf('155098','%2x%2x%2x',[1 3])/255, 'LineWidth',4)
     plot(time, crel_ratio(:,3),'Color',sscanf('C4005B','%2x%2x%2x',[1 3])/255, 'LineWidth',4)
@@ -290,10 +293,9 @@ if run_poly
     if alpha_mut %reduced IkBa induced synthesis by Rela
         p_mod = [
             6 1 1.5e-5];
-    elseif epsilon_KO %set basal and induced synthesis to zero (hence no IkBe in system)
+    elseif epsilon_KO %set translation to zero (hence no IkBe in system)
         p_mod = [
-            27 1 0
-            28 1 0];
+            30 1 0];
     elseif modify_IkBeRelA
         p_mod = [
             59 1 0.00003576
@@ -329,19 +331,17 @@ if run_poly
     rela_cyto = sum(output_poly(:,1:5),2);
     rela_nuc = sum(output_poly(:,6:8),2);
     rela(:,4) = rela_nuc;
-    %rela_cyto = rela_cyto(1:size(rela_ratio,1),:);
-    %rela_nuc = rela_nuc(1:size(rela_ratio,1),:);
     rela_total = (3.5*rela_cyto+rela_nuc)/4.5;
     rela_ratio(:,4) = rela_nuc/(rela_total(1)+untran_r);
+    rela_ratio(:,4) = rela_ratio(:,4) - rela_ratio(1,4);
 
     %cRel Calculations
     crel_cyto = sum(output_poly(:,9:13),2);
     crel_nuc = sum(output_poly(:,14:16),2);
     crel(:,4) = crel_nuc;
-    %crel_cyto = crel_cyto(1:size(crel_ratio,1),:);
-    %crel_nuc = crel_nuc(1:size(crel_ratio,1),:);
     crel_total = (3.5*crel_cyto+crel_nuc)/4.5;
     crel_ratio(:,4) = crel_nuc/(crel_total(1)+untran_c);
+    crel_ratio(:,4) = crel_ratio(:,4) - crel_ratio(1,4);
     
     title('Poly(I:C) Simulation')
     xlabel('Time(hr)')
@@ -361,10 +361,9 @@ if run_fla
    if alpha_mut %reduced IkBa induced synthesis by Rela
         p_mod = [
             6 1 1.5e-5];
-    elseif epsilon_KO %set basal and induced synthesis to zero (hence no IkBe in system)
+    elseif epsilon_KO %set translation to zero (hence no IkBe in system)
         p_mod = [
-            27 1 0
-            28 1 0];
+            30 1 0];
     elseif modify_IkBeRelA
         p_mod = [
             59 1 0.00003576
@@ -403,6 +402,7 @@ if run_fla
     rela(:,5) = rela_nuc;
     rela_total = (3.5*rela_cyto+rela_nuc)/4.5;
     rela_ratio(:,5) = rela_nuc/(rela_total(1)+untran_r);
+    rela_ratio(:,5) = rela_ratio(:,5) - rela_ratio(1,5);
 
     %cRel Calculations
     crel_cyto = sum(output_fla(:,9:13),2);
@@ -410,11 +410,12 @@ if run_fla
     crel(:,5) = crel_nuc;
     crel_total = (3.5*crel_cyto+crel_nuc)/4.5;
     crel_ratio(:,5) = crel_nuc/(crel_total(1)+untran_c);
+    crel_ratio(:,5) = crel_ratio(:,5) - crel_ratio(1,5);
     
     title('Flagellin Simulation')
     xlabel('Time(hr)')
     ylabel('Nuclear/Total Cell Intensity')
-    axis([0 options.SIM_TIME/60 0 2.5])
+    axis([0 options.SIM_TIME/60 0 2])
 
     plot(time, rela_ratio(:,5),'Color',sscanf('155098','%2x%2x%2x',[1 3])/255, 'LineWidth',4)
     plot(time, crel_ratio(:,5),'Color',sscanf('C4005B','%2x%2x%2x',[1 3])/255,'LineWidth',4)
@@ -429,22 +430,21 @@ if run_r84
    if alpha_mut %reduced IkBa induced synthesis by Rela
         p_mod = [
             6 1 1.5e-5];
-    elseif epsilon_KO %set basal and induced synthesis to zero (hence no IkBe in system)
+   elseif epsilon_KO %set translation to zero (hence no IkBe in system)
         p_mod = [
-            27 1 0
-            28 1 0];
-    elseif modify_IkBeRelA
+            30 1 0];
+   elseif modify_IkBeRelA
         p_mod = [
             59 1 0.00003576
             61 1 0.00003576];
-    elseif modify_IKKIkBe
+   elseif modify_IKKIkBe
         p_mod = [
             45 1 9.228
             46 1 9.228
             65 1 9.228];
-    else
+   else
         p_mod = [];
-    end
+   end
 
     doses = [350]; %350 ng/mL; R848 = 314 g/mol
     dose_scale = 1/314; % convert ng/mL to uM
@@ -471,6 +471,7 @@ if run_r84
     rela(:,6) = rela_nuc;
     rela_total = (3.5*rela_cyto+rela_nuc)/4.5;
     rela_ratio(:,6) = rela_nuc/(rela_total(1)+untran_r);
+    rela_ratio(:,6) = rela_ratio(:,6) - rela_ratio(1,6);
 
     %cRel Calculations
     crel_cyto = sum(output_r84(:,9:13),2);
@@ -478,11 +479,12 @@ if run_r84
     crel(:,6) = crel_nuc;
     crel_total = (3.5*crel_cyto+crel_nuc)/4.5;
     crel_ratio(:,6) = crel_nuc/(crel_total(1)+untran_c);
+    crel_ratio(:,6) = crel_ratio(:,6) - crel_ratio(1,6);
     
     title('R848 Simulation')
     xlabel('Time(hr)')
     ylabel('Nuclear/Total Cell Intensity')
-    axis([0 options.SIM_TIME/60 0 2.5])
+    axis([0 options.SIM_TIME/60 0 2])
     
     plot(time, rela_ratio(:,6),'Color',sscanf('155098','%2x%2x%2x',[1 3])/255, 'LineWidth',4)
     plot(time, crel_ratio(:,6),'Color',sscanf('C4005B','%2x%2x%2x',[1 3])/255,'LineWidth',4)
@@ -497,10 +499,9 @@ if run_pam
     if alpha_mut %reduced IkBa induced synthesis by Rela
         p_mod = [
             6 1 1.5e-5];
-    elseif epsilon_KO %set basal and induced synthesis to zero (hence no IkBe in system)
+    elseif epsilon_KO %set translation to zero (hence no IkBe in system)
         p_mod = [
-            27 1 0
-            28 1 0];
+            30 1 0];
     elseif modify_IkBeRelA
         p_mod = [
             59 1 0.00003576
@@ -539,28 +540,30 @@ if run_pam
     rela_cyto = sum(output_pam(:,1:5),2);
     rela_nuc = sum(output_pam(:,6:8),2);
     rela(:,7) = rela_nuc;
-    rela_cyto = [repmat(rela_cyto(1,1),75,1);rela_cyto];
-    rela_nuc = [repmat(rela_nuc(1,1),75,1);rela_nuc];
+    rela_cyto = [repmat(rela_cyto(1,1),90,1);rela_cyto];
+    rela_nuc = [repmat(rela_nuc(1,1),90,1);rela_nuc];
     rela_cyto = rela_cyto(1:size(rela_ratio,1),:);
     rela_nuc = rela_nuc(1:size(rela_ratio,1),:);
     rela_total = (3.5*rela_cyto+rela_nuc)/4.5;
     rela_ratio(:,7) = rela_nuc/(rela_total(1)+untran_r);
+    rela_ratio(:,7) = rela_ratio(:,7) - rela_ratio(1,7);
 
     %cRel Calculations
     crel_cyto = sum(output_pam(:,9:13),2);
     crel_nuc = sum(output_pam(:,14:16),2);
     crel(:,7) = crel_nuc;
-    crel_cyto = [repmat(crel_cyto(1,1),75,1);crel_cyto];
-    crel_nuc = [repmat(crel_nuc(1,1),75,1);crel_nuc];
+    crel_cyto = [repmat(crel_cyto(1,1),90,1);crel_cyto];
+    crel_nuc = [repmat(crel_nuc(1,1),90,1);crel_nuc];
     crel_cyto = crel_cyto(1:size(crel_ratio,1),:);
     crel_nuc = crel_nuc(1:size(crel_ratio,1),:);
     crel_total = (3.5*crel_cyto+crel_nuc)/4.5;
     crel_ratio(:,7) = crel_nuc/(crel_total(1)+untran_c);
+    crel_ratio(:,7) = crel_ratio(:,7) - crel_ratio(1,7);
     
     title('Pam3CSK Simulation')
     xlabel('Time(hr)')
     ylabel('Nuclear/Total Cell Intensity')
-    axis([0 options.SIM_TIME/60 0 2.5])
+    axis([0 options.SIM_TIME/60 0 2])
 
     plot(time, rela_ratio(:,7),'Color',sscanf('155098','%2x%2x%2x',[1 3])/255, 'LineWidth',4)
     plot(time, crel_ratio(:,7),'Color',sscanf('C4005B','%2x%2x%2x',[1 3])/255,'LineWidth',4)
